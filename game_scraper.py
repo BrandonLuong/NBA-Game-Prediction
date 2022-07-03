325a686491a51c3919226a733b3e79126c5d9330
Brandon Luong <brandonluong@MacBook-Pro-174.attlocal.net>
2022-07-02 23:38:55 -0700

    second commit

--- game_scraper.py
+++ game_scraper.py
@@ -1,9 +1,27 @@
 from bs4 import BeautifulSoup
+from bs4 import Comment
 import pandas
 import requests
+import time
+from selenium import webdriver as wd
+import chromedriver_binary
 
-url = 'https://www.nba.com/stats/lineups/traditional/?Season=2021-22&SeasonType=Regular%20Season'
+url = 'https://www.nba.com/stats/teams/boxscores/?Season=2021-22&SeasonType=Regular%20Season'
 
-#load webpage
-r = requests.get(url)
-print(r)
+driver = wd.Chrome()
+driver.get(url)
+
+# #load webpage
+r = requests.get(url).text
+soup = BeautifulSoup(r, 'lxml')
+
+driver.find_element_by_xpath()
+
+
+
+# comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+# # stat_table = soup.find('div', class_='row row5 collapse stats-filters-top')
+# for comment in comments:
+#     print(comment)
+#     comment.extract()
+# print(soup.prettify)
