c77b9074a26d353aed5d2170bbf4bc40eebc8342
Brandon Luong <brandonluong@MacBook-Pro-174.attlocal.net>
2022-07-07 17:21:19 -0700

    collected games

--- game_scraper.py
+++ game_scraper.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import pandas
+import pandas as pd
 import requests
 import time
 from selenium import webdriver as wd
@@ -7,16 +7,27 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import chromedriver_binary
 
-url = 'https://www.nba.com/stats/teams/boxscores/?Season=2021-22&SeasonType=Regular%20Season'
+# def get_games():
+# start = 2021
+url = 'https://www.nba.com/stats/teams/boxscores/?Season=2019-20&SeasonType=Regular%20Season'
+
+
+# url = 'https://www.nba.com/stats/teams/boxscores/?Season=' + str(start) + '-' + str(start - 2000 + 1) + '&SeasonType=Regular%20Season'
+# start -= 1
 
 driver = wd.Chrome()
 driver.get(url)
 
-select = Select(driver.find_element(By.XPATH, "/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select"))
+# select_season = Select(driver.find_element(By.XPATH, "/html/body/main/div/div/div[2]/div/div/div[1]/div[1]/div/div/label/select"))
+
+select_page = Select(driver.find_element(By.XPATH, "/html/body/main/div/div/div[2]/div/div/nba-stat-table/div[1]/div/div/select"))
+
+driver.implicitly_wait(1)
+# select_season.select_by_index(i + 2)
 
 driver.implicitly_wait(1)
 #select "All" option
-select.select_by_index(0)
+select_page.select_by_index(0)
 
 source = driver.page_source
 soup = BeautifulSoup(source, 'lxml')
@@ -37,3 +48,21 @@ for tr in table_rows:
     row = [tr.text.strip() for tr in td]
     row_list.append(row)
 
+df = pd.DataFrame(row_list, columns=headers_list)
+df.to_csv('games_stats.csv', mode='a', header=False)
+
+
+# start = 2021
+
+# url = 'https://www.nba.com/stats/teams/boxscores/?Season=' + str(start) + '-' + str(start - 2000 + 1) + '&SeasonType=Regular%20Season'
+# #     start -= 1
+
+# driver = wd.Chrome()
+# driver.get(url)
+
+# select = Select(driver.find_element(By.XPATH, "/html/body/main/div/div/div[2]/div/div/div[1]/div[1]/div/div/label/select"))
+
+# driver.implicitly_wait(1)
+# #select "All" option
+# select.select_by_index(3)
+# time.sleep(5)
